70faf6fe1c2e7a1e3a9b27ea91381dc1975aa99f
Paloma <paloma.a.silva@capgemini.com>
2022-08-28 19:23:58 -0300

    PaymentController

---
+++ src/main/java/br/com/alurafood/payments/controller/PaymentController.java
@@ -0,0 +1,62 @@
+package br.com.alurafood.payments.controller;
+
+
+import br.com.alurafood.payments.dto.PaymentDto;
+import br.com.alurafood.payments.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.net.URI;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+
+    @GetMapping
+    public Page<PaymentDto> list(@PageableDefault(size = 10) Pageable pageable) {
+        return paymentService.getAll(pageable);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PaymentDto> details(@PathVariable @NotNull Long id) {
+        PaymentDto dto = paymentService.getId(id);
+
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<PaymentDto> registration(@RequestBody @Valid PaymentDto paymentDto, UriComponentsBuilder uriBuilder) {
+        PaymentDto payment = paymentService.createPayment(paymentDto);
+
+        URI address = uriBuilder.path("/payments/{id}").buildAndExpand(payment.getId()).toUri();
+
+        return ResponseEntity.created(address).body(payment);
+    }
+
+    @PutMapping
+    public ResponseEntity<PaymentDto> update(@PathVariable @NotNull Long id, @RequestBody @Valid PaymentDto paymentDto, UriComponentsBuilder uriBuilder) {
+        PaymentDto payment = paymentService.updatePayment(id, paymentDto);
+
+        URI address = uriBuilder.path("/payments/{id}").buildAndExpand(payment.getId()).toUri();
+
+        return ResponseEntity.created(address).body(payment);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<PaymentDto> delete(@PathVariable @NotNull Long id) {
+        paymentService.deletePayment(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
